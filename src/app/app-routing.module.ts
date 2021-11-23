173e6c053dc97d2455a0410cc148b378d7872dc3
Albert <49212593+Albert-HR@users.noreply.github.com>
2021-11-23 11:20:36 -0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployersFormComponent } from './components/employers-form/employers-form.component';
+import { EmployersListComponent } from './components/employers-list/employers-list.component';
+import { EmployeesPageComponent } from './pages/employees-page/employees-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PatientPageComponent } from './pages/patient-page/patient-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  {
+    path: 'employees', component: EmployeesPageComponent, children: [
+      { path: 'list', component: EmployersListComponent },
+      { path: 'create', component: EmployersFormComponent },
+    ]
+  },
+  { path: 'patients', component: PatientPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { EmployeesPageComponent } from './pages/employees-page/employees-page.component';
+import { PatientPageComponent } from './pages/patient-page/patient-page.component';
+import { EmployersListComponent } from './components/employers-list/employers-list.component';
+import { EmployersFormComponent } from './components/employers-form/employers-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    EmployeesPageComponent,
+    PatientPageComponent,
+    EmployersListComponent,
+    EmployersFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
