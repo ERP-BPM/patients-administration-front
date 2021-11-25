3776fdbb12c12fb3bd74f3ab94c62bc14ff0f5f5
Albert <49212593+Albert-HR@users.noreply.github.com>
2021-11-24 18:55:44 -0600

    new design

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,8 +1,8 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
-import {setContext} from '@apollo/client/link/context';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
 
 const uri = 'http://5.161.52.247:8888/v1/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
@@ -12,12 +12,12 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     const token = localStorage.getItem('token');
     return {
       headers: {
-        'x-hasura-access-key': ''
+        'x-hasura-access-key': 'contrAsenAAERPCAMUNDADocker1424321'
       }
     };
   });
 
-  const link = ApolloLink.from([auth, httpLink.create({uri})]);
+  const link = ApolloLink.from([auth, httpLink.create({ uri })]);
   // const cache = new InMemoryCache();
 
   return {
