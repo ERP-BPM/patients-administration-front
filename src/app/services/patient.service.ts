d779669f18fdf28fa1957e861d1cd01b41becafa
Albert <49212593+Albert-HR@users.noreply.github.com>
2021-11-24 22:10:14 -0600

    patients resolvers

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -4,27 +4,44 @@ import { Apollo, gql } from 'apollo-angular';
 
 const CREATE_PATIENT = gql`
   mutation MyMutation(
-      $fecha_nacimiento: date,
-      $apellido_materno: String,
-      $apellido_paterno: String,
-      $curp: String,
-      $estado_civil: String,
-      $email: String,
-      $genero: String,
-      $nombre: String
-    ) {
-      insert_Hospital_persona(objects: {apellido_materno: $apellido_materno,
-      apellido_paterno: $apellido_paterno,
-      curp: $curp,
-      email: $email,
-      estado_civil: $estado_civil,
-      fecha_nacimiento: $fecha_nacimiento,
-      genero: $genero,
-      nombre: $nombre,
-  ) {
-      affected_rows
-    }
+    $fecha_nacimiento: date,
+    $apellido_materno: String,
+    $apellido_paterno: String,
+    $curp: String,
+    $estado_civil: String,
+    $genero: String,
+    $nombre: String, $grupo_sanguineo: String, $nss: String) {
+    insert_Hospital_paciente(objects: {apellido_materno: $apellido_materno, apellido_paterno: $apellido_paterno, curp: $curp, estado_civil: $estado_civil, fecha_nacimiento: $fecha_nacimiento, genero: $genero, nombre: $nombre, grupo_sanguineo: $grupo_sanguineo, nss: $nss}) {
+  affected_rows
   }
+  }
+`;
+
+const GET_PATIENTS = gql`
+  query MyQuery {
+  Hospital_paciente {
+    id_paciente
+    nss
+    grupo_sanguineo
+    apellido_materno
+    apellido_paterno
+    curp
+    estado_civil
+    fecha_nacimiento
+    genero
+    nombre
+  }
+}
+`
+
+const DELETE_PACIENT = gql`
+mutation MyMutation($id: uuid) {
+delete_Hospital_paciente(where: {id_paciente: {_eq: $id }}) {
+  returning {
+    nss
+  }
+}
+}
 `;
 
 @Injectable({
@@ -36,79 +53,55 @@ export class PatientService {
   patients: Patient[];
 
   constructor(private apollo: Apollo) {
-    this.patients = [
-      new Patient({
-        id: '1',
-        curp: 'CURP',
-        nombre: 'Erick Jared',
-        apellido_paterno: 'Corpus',
-        apellido_materno: 'Mendoza',
-        email: 'email',
-        estado_civil: 'estado_civil',
-        fecha_nacimiento: 'hoy',
-        genero: 'genero',
-        grupo_sanguineo: 'A',
-        nss: '874512875478D',
-      }),
-      new Patient({
-        id: '1',
-        curp: 'CURP',
-        nombre: 'Erick Jared',
-        apellido_paterno: 'Corpus',
-        apellido_materno: 'Mendoza',
-        email: 'email',
-        estado_civil: 'estado_civil',
-        fecha_nacimiento: 'hoy',
-        genero: 'genero',
-        grupo_sanguineo: 'A',
-        nss: '874512875478D',
-      }),
-      new Patient({
-        id: '1',
-        curp: 'CURP',
-        nombre: 'Erick Jared',
-        apellido_paterno: 'Corpus',
-        apellido_materno: 'Mendoza',
-        email: 'email',
-        estado_civil: 'estado_civil',
-        fecha_nacimiento: 'hoy',
-        genero: 'genero',
-        grupo_sanguineo: 'A',
-        nss: '874512875478D',
-      }),
-    ];
+    this.patients = [];
+
+    this.apollo.query({
+      query: GET_PATIENTS,
+    }).subscribe(({ data }) => {
+      const dt = data['Hospital_paciente'];
+
+      for (const p of dt) {
+        this.patients.push(new Patient({
+          id: p.id_paciente,
+          curp: p.curp,
+          nombre: p.nombre,
+          apellido_paterno: p.apellido_paterno,
+          apellido_materno: p.apellido_materno,
+          email: p.email,
+          estado_civil: p.estado_civil,
+          genero: p.genero,
+          fecha_nacimiento: 'hoy',
+          grupo_sanguineo: p.grupo_sanguineo,
+          nss: p.nss,
+        }));
+      }
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+    });
   }
 
   get getPatients(): Patient[] {
     return this.patients;
   }
 
-  deletePatient(patient: Patient): void {
-    const index: number = this.patients.indexOf(patient);
-    if (index !== -1) {
-      this.patients.splice(index, 1);
-    }
-  }
-
   createPatient(data: any): void {
-    /*
-    this.patients.push(new Patient({
-      id: `${this.patients.length + 1}`,
-      curp: data.curp,
-      nombre: data.nombre,
-      apellido_paterno: data.apellido_paterno,
-      apellido_materno: data.apellido_materno,
-      email: data.email,
-      estado_civil: data.estado_civil,
-      genero: data.genero,
-      fecha_nacimiento: 'hoy',
-      grupo_sanguineo: data.grupo_sanguineo,
-      nss: data.nss,
-    }));*/
     this.apollo.mutate({
       mutation: CREATE_PATIENT,
       variables: {
-        //id_persona: '0587796e-bee6-45af-a53b-e3937f398daa', // `${this.patients.length + 1}`,
+        curp: data.curp,
+        nombre: data.nombre,
+        apellido_paterno: data.apellido_paterno,
+        apellido_materno: data.apellido_materno,
+        // email: data.email,
+        estado_civil: data.estado_civil,
+        genero: data.genero,
+        fecha_nacimiento: '2021-11-11',
+        grupo_sanguineo: data.grupo_sanguineo,
+        nss: data.nss,
+      }
+    }).subscribe(({ }) => {
+      this.patients.push(new Patient({
+        id: data.id_paciente,
         curp: data.curp,
         nombre: data.nombre,
         apellido_paterno: data.apellido_paterno,
@@ -116,12 +109,28 @@ export class PatientService {
         email: data.email,
         estado_civil: data.estado_civil,
         genero: data.genero,
-        fecha_nacimiento: '2021-11-11',
-        // grupo_sanguineo: data.grupo_sanguineo,
-        // nss: data.nss,
+        fecha_nacimiento: 'hoy',
+        grupo_sanguineo: data.grupo_sanguineo,
+        nss: data.nss,
+      }));
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
+
+  deletePatient(patient: Patient): void {
+    this.apollo.mutate({
+      mutation: DELETE_PACIENT,
+      variables: {
+        id: patient.id
       }
     }).subscribe(({ data }) => {
-      console.log('got data', data);
+      console.log(data);
+
+      const index: number = this.patients.indexOf(patient);
+      if (index !== -1) {
+        this.patients.splice(index, 1);
+      }
     }, (error) => {
       console.log('there was an error sending the query', error);
     });
