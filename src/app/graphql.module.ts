86fea134c04359909bbe9d7c2b833f66a5550b24
Albert <49212593+Albert-HR@users.noreply.github.com>
2021-11-30 11:25:38 -0600

    restore

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,7 +4,7 @@ import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/c
 import { HttpLink } from 'apollo-angular/http';
 import { setContext } from '@apollo/client/link/context';
 
-const uri = '/api/v1/graphql';
+const uri = 'http://5.161.52.247:8888/v1/graphql';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
 
 
